bc15e6b64954efdf2db8499f2175fd7e0978e997
Raghunadh Hital <103263301+Raghunadh9@users.noreply.github.com>
2024-10-06 19:50:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,10 @@ import React, { useEffect, useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import axios from "axios";
 import Header from "@/components/header";
-import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
 import {
   AlertDialog,
@@ -36,12 +34,10 @@ interface DataSource {
 }
 
 const Page: React.FC = () => {
-  let [textName, setTextName] = useState<string>("");
+  const [textName, setTextName] = useState<string>("");
   const [dataSource, setDataSource] = useState<DataSource | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-  const searchParams = useSearchParams();
-  let search = searchParams.get("name");
-  const router = useRouter();
+
   const saveHandler = async (dataSource: DataSource) => {
     setLoading(true);
     await axios
@@ -60,69 +56,33 @@ const Page: React.FC = () => {
         setLoading(false);
         toast.success("Name successfully added to Database ⭐!");
       })
-      .catch((err) => toast.error(err));
+      .catch((err) => {
+        setLoading(false);
+        toast.error(err);
+      });
   };
 
-  useEffect(() => {
-    // Set initial value only if it's different from the current state
-    if (search !== null && search !== textName) {
-      setTextName(search);
-    }
-  }, [search, textName]);
-
-  const inputFocusRef = useRef(null);
-  const buttonRef = useRef(null);
-  useEffect(() => {
-    inputFocusRef.current.focus();
-    if (search !== null) {
-      if (buttonRef.current) {
-        buttonRef.current.click();
-      }
-    }
-  }, []);
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    // Making an API request without updating the URL
     axios
       .get(
         `https://weljon.com/convert?name=${textName
-          .replaceAll(".", "")
-          .replaceAll(",", "")
-          .replaceAll(" - ", "")
-          .replaceAll("-", " ")
-          .replaceAll(";", "")
-          .replaceAll("(", "")
-          .replaceAll(" & ", "")
-          .replaceAll(")", "")
-          .replaceAll("/", "")
-          .replaceAll(":", "")
-          .replaceAll("  ", " ")
-          .replaceAll("[", "")
-          .replaceAll("]", "")}`
+          .replace(/[.,;()&/:]/g, "") // Remove unwanted characters
+          .replace(/\s+/g, " ") // Replace multiple spaces with a single space
+          .trim()}` // Remove leading and trailing spaces
       )
-      .then((res) => setDataSource(res.data));
+      .then((res) => setDataSource(res.data))
+      .catch((err) => toast.error(err));
   };
 
-  const updateURL = (newTextName: string) => {
-    // Update the URL without a page reload
-    router.push(
-      `?name=${encodeURIComponent(
-        newTextName
-          .replaceAll(".", "")
-          .replaceAll(",", "")
-          .replaceAll(" - ", "")
-          .replaceAll("-", " ")
-          .replaceAll(";", "")
-          .replaceAll("&", " ")
-          .replaceAll("/", "")
-          .replaceAll(":", "")
-          .replaceAll("(", "")
-          .replaceAll(")", "")
-          .replaceAll("  ", " ")
-          .replaceAll("[", "")
-          .replaceAll("]", "")
-      )}`
-    );
-  };
+  const inputFocusRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    inputFocusRef.current.focus();
+  }, []);
 
   return loading ? (
     <div className="flex items-center justify-center h-screen bg-gray-100">
@@ -131,45 +91,23 @@ const Page: React.FC = () => {
   ) : (
     <div className="">
       <Header />
-
       <div>
         <form
           className="container gap flex justify-between"
-          onSubmit={(e) => submitHandler(e)}
+          onSubmit={submitHandler}
         >
           <Input
             type="text"
             placeholder="Name"
             className="w-[70%]"
-            value={textName
-              .replaceAll(".", "")
-              .replaceAll(",", "")
-              .replaceAll(" - ", "")
-              .replaceAll("-", " ")
-              .replaceAll(";", "")
-              .replaceAll("(", "")
-              .replaceAll(")", "")
-              .replaceAll("&", " ")
-              .replaceAll("/", "")
-              .replaceAll(":", "")
-              .replaceAll("  ", " ")
-              .replaceAll("[", "")
-              .replaceAll("]", "")}
+            value={textName} // Keep this value bound to textName state
             ref={inputFocusRef}
-            onChange={(e) => {
-              const newValue = e.target.value;
-              setTextName(newValue);
-              console.log(textName);
-              updateURL(newValue);
-            }}
+            onChange={(e) => setTextName(e.target.value)} // Update state on input change
           />
 
           <Button
             type="submit"
             ref={buttonRef}
-            // onClick={
-            //   () => updateURL(textName) // Update the URL when input changes
-            // }
             disabled={textName.length === 0}
           >
             Calculate <ArrowRight className="ml-2 h-4 w-4" />
@@ -181,35 +119,35 @@ const Page: React.FC = () => {
           <div className="container mt-[30px] flex items-start justify-start">
             <table
               border={2}
-              className="border-4dark:border-white border-yellow-600"
+              className="border-4 dark:border-white border-yellow-600"
             >
               <thead className="border-4 dark:border-white border-yellow-600 p-[20px]">
                 <tr className="border-4 dark:border-white border-yellow-600">
-                  <th className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white  text-2xl font-[900]">
+                  <th className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white text-2xl font-[900]">
                     Grp.
                   </th>
-                  <th className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white  text-2xl font-[900]">
+                  <th className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white text-2xl font-[900]">
                     Name
                   </th>
-                  <th className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white  text-2xl font-[900]">
+                  <th className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white text-2xl font-[900]">
                     Total
                   </th>
-                  <th className="border-4  dark:border-white border-yellow-600 bg-green-600 text-white  text-2xl font-[900]">
+                  <th className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white text-2xl font-[900]">
                     V
                   </th>
-                  <th className="border-4px-[10px]  dark:border-white border-yellow-600 bg-green-600 text-white  text-2xl font-[900] text-2xl">
+                  <th className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white text-2xl font-[900]">
                     C
                   </th>
                 </tr>
               </thead>
               <tbody className="border-4 dark:border-white border-yellow-600 text-[#960084] font-[900]">
                 <tr className="border-4 dark:border-white border-yellow-600">
-                  <td className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white  text-2xl ">
+                  <td className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white text-2xl ">
                     C
                   </td>
-                  <td className=" border-4 dark:border-white border-yellow-600 ">
+                  <td className="border-4 dark:border-white border-yellow-600">
                     <table
-                      className=" table-padding text-2xl"
+                      className="table-padding text-2xl"
                       dangerouslySetInnerHTML={{
                         __html:
                           typeof dataSource !== "undefined" &&
@@ -222,21 +160,19 @@ const Page: React.FC = () => {
                     {dataSource?.g2tot}
                   </td>
                   <td className="border-4 dark:border-white border-yellow-600 text-2xl bg-green-600 text-white ">
-                    {" "}
                     {dataSource?.g2vtot}
                   </td>
                   <td className="border-4 dark:border-white border-yellow-600 text-2xl bg-green-600 text-white ">
-                    {" "}
                     {dataSource?.g2nettot}
                   </td>
                 </tr>
                 <tr className="border-4 dark:border-white border-yellow-600">
-                  <td className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white  text-2xl ">
+                  <td className="border-4 dark:border-white border-yellow-600 bg-green-600 text-white text-2xl ">
                     P
                   </td>
-                  <td className=" border-4 dark:border-white border-yellow-600">
+                  <td className="border-4 dark:border-white border-yellow-600">
                     <table
-                      className=" table-padding text-2xl"
+                      className="table-padding text-2xl"
                       dangerouslySetInnerHTML={{
                         __html:
                           typeof dataSource !== "undefined" &&
@@ -249,83 +185,40 @@ const Page: React.FC = () => {
                     {dataSource?.g3tot}
                   </td>
                   <td className="border-4 dark:border-white border-yellow-600 text-2xl bg-green-600 text-white ">
-                    {" "}
                     {dataSource?.g3vtot}
                   </td>
                   <td className="border-4 dark:border-white border-yellow-600 text-2xl bg-green-600 text-white ">
-                    {" "}
                     {dataSource?.g3nettot}
                   </td>
                 </tr>
               </tbody>
             </table>
           </div>
-          <div className="container flex  items-start mt-[10px] justify-between">
-            {" "}
-            <span className="text-green-600  text-2xl font-[900]">
-              Total Letters - {dataSource?.tot_letters}
-            </span>
-            <div className="">
-              {/* <Button variant={"default"} className="mr-[10px]"> */}
-              <AlertDialog>
-                <AlertDialogTrigger>
-                  <Button variant={"default"} className="mr-[10px]">
+          <div className="text-center flex flex-col items-center mt-3">
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button>Save</Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This will save your name in the database. You will be able
+                    to see it in the <Link href={"/saved"}>saved</Link> section.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction onClick={() => saveHandler(dataSource)}>
                     Save
-                  </Button>
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>
-                      Are you absolutely sure?
-                    </AlertDialogTitle>
-                    <AlertDialogDescription>
-                      Do you want to save this name -{" "}
-                      <b>{textName.toUpperCase()}</b> ?
-                    </AlertDialogDescription>
-                  </AlertDialogHeader>
-                  <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction onClick={() => saveHandler(dataSource)}>
-                      Continue
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-              {/* </Button> */}
-              <Link href={"/"}>
-                <Button
-                  variant={"destructive"}
-                  onClick={() => {
-                    setTextName("");
-                    setDataSource(null);
-                  }}
-                >
-                  Cancel
-                </Button>
-              </Link>
-            </div>
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
           </div>
         </>
-      ) : (
-        <>
-          <div className="container flex justify-center align-middle">
-            <h1>Please write your name to get started...</h1>
-          </div>
-        </>
-      )}
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-      {/* <Footer /> */}
+      ) : null}
+      <ToastContainer />
     </div>
   );
 };
